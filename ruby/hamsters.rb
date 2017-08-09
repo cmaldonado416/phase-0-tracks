ef0cdba2807e630d88b014f9c9b2b63c8d413b8a
Cynthia Maldonado <cmaldonado416@gmail.com>
2017-08-08 23:45:10 -0400

    Placed comments for easier readibility

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,16 @@
+#ask user for hamster name
 puts "What is the hamster's name"
 hamster_name= gets.chomp
 puts "The hamster's name is #{hamster_name}"
-
+#ask user for volume level 1-10
 puts "What is the volume level: 1 to 10"
 volume_level= gets.to_i
 puts "The volume level is #{volume_level}"
-
+#ask user for fur color
 puts "What is the fur color of the hamster?"
 fur_color= gets.chomp
 puts "The fur color is #{fur_color}"
-
+#ask user if good candidate
 puts "Is this hamster a good candidate for adoption? yes or no"
 adoption= gets.chomp
 
@@ -19,7 +20,7 @@ elsif adoption == "no"
   puts "This hamster is not a good candidate for adoption"
 end
 
-
+#ask user for estimated age
 puts "What is the estimated age of this hamster?"
 age= gets.chomp
 
