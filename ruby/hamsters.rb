b80d0d2770c887ba667c02ab841559403bc7a399
Cynthia Maldonado <cmaldonado416@gmail.com>
2017-08-08 23:10:13 -0400

    Add variables for Release 1 do the thing

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,1 +1,20 @@
 puts "What is the hamster's name"
+hamster_name= gets.chomp
+puts "The hamster's name is #{hamster_name}"
+
+puts "What is the volume level: 1 to 10"
+volume_level= gets.chomp
+puts "The volume level is #{volume_level}"
+
+puts "What is the fur color of the hamster?"
+fur_color= gets.chomp
+puts "The fur color is #{fur_color}"
+
+puts "Is this hamster a good candidate for adoption? yes or no"
+good_bad= gets.chomp
+
+puts "What is the estimated age of this hamster?"
+age= gets.chomp
+
+
+
