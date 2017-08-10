324f82d42c20c5133fa336ca9cd6eff117786a6b
Cynthia Maldonado <cmaldonado416@gmail.com>
2017-08-09 22:00:13 -0400

    Add pseudo-commit

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,1 +1,7 @@
-1. we want a method that advances the letter of string forward by writing an encrypt method
+#we want a method that advances the letter of string forward by writing an encrypt method (.encrypt)
+
+#define some variables, with a string<sgspog= "">
+
+#we will use += 1 to advance the variable defined
+
+encrypted_word = "abc"
