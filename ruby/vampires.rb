356e60bd3f2f715a821c15f95fff2d22bf9e88c8
Cynthia Maldonado <cmaldonado416@gmail.com>
2017-08-17 00:28:50 -0400

    Initial commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,22 @@
+# what is your name
+puts "What is your name?"
+name = gets.chomp
+
+#how old are you? What year were you born?
+
+puts "How old are you?"
+age = gets.to_i
+
+puts "What year were you born?"
+year_born = gets.to_i
+
+#Our company cafeteria serves garlic bread, should we order some for you?
+
+puts "Our company cafeteria serves garlic bread, should we order some for you? (yes or no)"
+garlic = gets.chomp
+
+#would you like to enroll in the company's health insurance?
+
+puts "Would you like to enroll in the company's health insurance? (yes or no)"
+health_insurance = gets.chomp
+
